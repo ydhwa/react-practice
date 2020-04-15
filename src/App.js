62d61a95b709918cda5b9309c02a0a2fbf043254
YDH <ydhwa_18@naver.com>
2020-04-15 16:24:35 +0900

    PropTypes - type check

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent>리액트</MyComponent>;
+  return <MyComponent name={"react"}>리액트</MyComponent>;
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,19 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-// const MyComponent = props => {
-//   const { name, children } = props;
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다.
       <br />
       children 값은 {children}입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
 
 MyComponent.defaultProps = {
   name: "기본 이름"
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  // isRequired: 필수값 지정(없으면 콘솔에 오류메시지 나옴)
+  favoriteNumber: PropTypes.number.isRequired
 };
 
 // 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정한다.
