ac1d0a8cbe0cd642aa882ebfaa2663c6069a32c2
YDH <ydhwa_18@naver.com>
2020-04-15 16:01:48 +0900

    Props and defaultProps

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const MyComponent = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = props => {
+  return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
+};
+
+MyComponent.defaultProps = {
+  name: "기본 이름"
 };
 
 // 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정한다.
