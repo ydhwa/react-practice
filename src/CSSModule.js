2202d216601bb3bc734852fe406830fdcf8aeaab
YDH <ydhwa_18@naver.com>
2020-04-19 16:12:55 +0900

    Component styling - CSS module classnames

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,5 +1,8 @@
 import React from "react";
+import classNames from "classnames/bind";
 import styles from "./CSSModule.module.css";
+
+const cx = classNames.bind(styles); // 미리 styles에서 클래스를 받아 오도록 설정하고
 
 const CSSModule = () => {
   return (
@@ -9,7 +12,10 @@ const CSSModule = () => {
     // 1. 템플릿 리터럴
     // <div className={`${styles.wrapper} ${styles.inverted}`}>
     // 2. 배열, join 이용
-    <div className={[styles.wrapper, styles.inverted].join(" ")}>
+    // <div className={[styles.wrapper, styles.inverted].join(" ")}>
+
+    // classnames 이용
+    <div className={cx("wrapper", "inverted")}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
