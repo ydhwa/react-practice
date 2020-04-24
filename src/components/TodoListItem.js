0b2b32ae298a2f7aae0c9bc28faf29e21ec5f485
YDH <ydhwa_18@naver.com>
2020-04-24 22:33:36 +0900

    Add todo insert, view todos

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,14 +4,18 @@ import {
   MdCheckBox,
   MdRemoveCircleOutline,
 } from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
+import { dns } from '../../node_modules/address/lib/address';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
