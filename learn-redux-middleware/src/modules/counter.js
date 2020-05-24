6c8f55ec2cfdd3923c242b29a9ab8a38896300ee
YDH <ydhwa_18@naver.com>
2020-05-25 00:04:48 +0900

    Redux middleware - use redux-saga

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,22 +1,36 @@
 import { createAction, handleActions } from "redux-actions";
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
+const INCREASE_ASYNC = "counter/INCREASE_ASYNC";
+const DECREASE_ASYNC = "counter/DECREASE_ASYNC";
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
+// 마우스 클릭 이벤트가 payload 안에 들어가지 않도록
+// () => undefined를 두 번쨰 파라미터로 넣는다.
+export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
+export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
+
+function* increaseSaga() {
+  yield delay(1000);
+  yield put(increase());
+}
+
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* counterSaga() {
+  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해 줌
+  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // takeLease는 기존에 진행 중이던 작업이 있다면 취소 처리하고
+  // 가장 마지막으로 실행된 작업만 수행
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
 
 const initialState = 0; // 상태는 반드시 객체일 필요가 없다.
 
--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,21 +1,69 @@
-import { handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
+import { call, put, takeLatest } from "redux-saga/effects";
 import * as api from "../lib/api";
-import createRequestThunk from "../lib/createRequestThunk";
+import { startLoading, finishLoading } from "./loading";
 
 // 액션 타입 선언
 // 한 요청당 세 개 만들어야 함
 
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 // thunk 함수 생성
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 떄 다른 액션을 디스패치함
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+  yield put(startLoading(GET_POST));
+  // 파라미터로 action을 받아 오면 액션의 정보를 조회할 수 있다.
+  try {
+    // call을 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있다.
+    // 첫 번쨰 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수이다.
+    const post = yield call(api.getPost, action.payload); // =(api.getPost(action.payload))
+
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_POST));
+}
+
+function* getUsersSaga() {
+  yield put(startLoading(GET_USERS));
+  try {
+    const users = yield call(api.getUsers);
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 // 초기 상태 선언
 // 요청의 로딩 중 상태는 loading이라는 객체에서 관리
