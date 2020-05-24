52a455c275fcec766ceaf689a5d592a48b2dd413
YDH <ydhwa_18@naver.com>
2020-05-24 19:38:48 +0900

    Redix middleware - make middleware

---
+++ learn-redux-middleware/src/lib/loggerMiddleware.js
@@ -0,0 +1,20 @@
+const loggerMiddleware = (store) => (next) => (action) => {
+  // 미들웨어 기본 구조
+  console.group(action && action.type); // 액션 타입으로 log를 그룹화함
+  console.log("이전 상태", store.getState());
+  console.log("액션", action);
+  next(action); // 다음 미들웨어 혹은 리듀서에게 전달
+  console.log("다음 상태", store.getState()); // 업데이트된 상태
+  console.groupEnd(); // 그룹 끝
+};
+
+// 위 함수 풀어 쓴 버전
+// const loggerMiddleware = function loggerMiddleware(store) {
+//   return function (next) {
+//     return function (action) {
+//       // 미들웨어 기본 구조
+//     };
+//   };
+// };
+
+export default loggerMiddleware;
