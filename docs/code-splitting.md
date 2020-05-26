7beea207c5374e496a271c8e8ad59bf0e8ca8b35
YDH <ydhwa_18@naver.com>
2020-05-26 23:39:00 +0900

    Code splitting - use React.lazy and Suspense

--- docs/code-splitting.md
+++ docs/code-splitting.md
@@ -44,4 +44,33 @@ state를 사용하여 컴포넌트 코드 스플리팅을 하는 것이 어렵�
 
 ### React.lazy와 Suspense 사용하기
 
+React.lazy와 Suspense를 사용하면 코드 스플리팅을 하기 위해 state를 따로 선언하지 않고도 간편하게 코드 스플리팅이 가능하다.
+
+React.lazy는 컴포넌트를 렌더링하는 시점에서 비동기적으로 로딩할 수 있게 해주는 유틸 함수이다.
+
+사용법
+
+```javascript
+const SplitMe = React.lazy(() => import("./SplitMe"));
+```
+
+Suspense는 리액트 내장 컴포넌트로서 코드 스플리팅된 컴포넌트를 로딩하도록 발동시킬 수 있고, 로딩이 끝나지 않았을 때 보여 줄 UI를 설정할 수 있다.
+
+사용법
+
+```javascript
+import React, { Suspense } from "react";
+
+...
+<Suspense fallback={<div>loading...</div>}>
+  <SplitMe />
+</Suspense>;
+```
+
+Suspense에서 fallback props를 통해 로딩 중에 보여 줄 JSX를 지정할 수 있다.
+
+#### TIP
+
+크롬 개발자 도구 - Network 탭에서 Online을 누르면 셀렉트 박스가 뜨는데, 여기서 Slow 3G를 선택하면 느린 네트워크 속도를 시뮬레이트 할 수 있다.
+
 ## 정리
--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React, { useState, Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    SplitMe: null,
-  };
-  handleClick = async () => {
-    const loadedModule = await import("./SplitMe");
-    this.setState({
-      SplitMe: loadedModule.default,
-    });
+const SplitMe = React.lazy(() => import("./SplitMe"));
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   };
 
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello React!</p>
-          {SplitMe && <SplitMe />}
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
 }
 
 export default App;
